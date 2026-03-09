fcd8ca28d9e7e8ac16e7116ac10dea5e1fc9d6a5
H3A4K <alex.perlock@gmail.com>
2026-03-09 15:21:30 -0400

    9.1.1

--- js/work.js
+++ js/work.js
@@ -57,7 +57,7 @@ class SimpleDate {
         format.forEach((element) => {
             element = this.#remove_unneeded_chars(element);
             switch (element) {
-                case "d": case "m" : case "date":
+                case "d": case "m": case "date":
                     element = element == "m" ? "month" : "day";
 
                 case "day": case "date": case "month":
@@ -109,115 +109,129 @@ class Work {
 const items = [ // Work Items
     new Work(
         "Layout Assignment",
-        "Newspaper layout based on given wireframes", 
+        "Newspaper layout based on given wireframes",
         new SimpleDate(1, 2, 2026),
         "./work/layout/index.html"
     ),
 
     new Work(
-    "DOM arithmetic quiz 1 (Lab 4.2.1a)",
-    "JS DOM arithmetic quiz part 1",
-    new SimpleDate(1, 2, 2026),
-    "./work/Labs/4.2/1a/DOMquiz.html"
+        "DOM arithmetic quiz 1 (Lab 4.2.1a)",
+        "JS DOM arithmetic quiz part 1",
+        new SimpleDate(1, 2, 2026),
+        "./work/Labs/4.2/1a/DOMquiz.html"
     ),
 
     new Work(
-    "DOM arithmetic quiz 2 (Lab 4.2.1b)",
-    "JS DOM arithmetic quiz part 2",
-    new SimpleDate(1, 2, 2026),
-    "./work/Labs/4.2/1b/DOMquiz.html"
+        "DOM arithmetic quiz 2 (Lab 4.2.1b)",
+        "JS DOM arithmetic quiz part 2",
+        new SimpleDate(1, 2, 2026),
+        "./work/Labs/4.2/1b/DOMquiz.html"
     ),
 
     new Work(
-    "Movable elements (Lab 4.2.2)",
-    "Moving DOM elements using JS",
-    new SimpleDate(1, 2, 2026),
-    "./work/Labs/4.2/2/moveme.html"
+        "Movable elements (Lab 4.2.2)",
+        "Moving DOM elements using JS",
+        new SimpleDate(1, 2, 2026),
+        "./work/Labs/4.2/2/moveme.html"
     ),
 
     new Work(
-    "+/- arithmetic generator (Lab 4.1.1)",
-    "Creates 2 values between -100 and 100, and prompts the user for the answer",
-    new SimpleDate(2, 2, 2026),
-    "./work/Labs/4.1/arithmetic/index.html"
+        "+/- arithmetic generator (Lab 4.1.1)",
+        "Creates 2 values between -100 and 100, and prompts the user for the answer",
+        new SimpleDate(2, 2, 2026),
+        "./work/Labs/4.1/arithmetic/index.html"
     ),
 
     new Work(
-    "Lottery (Lab 4.1.2)",
-    "Lottery ball 'game'",
-    new SimpleDate(2, 2, 2026),
-    "./work/Labs/4.1/lottery/index.html"
+        "Lottery (Lab 4.1.2)",
+        "Lottery ball 'game'",
+        new SimpleDate(2, 2, 2026),
+        "./work/Labs/4.1/lottery/index.html"
     ),
 
     new Work(
-    "Catch the Rabbit (Lab 5.1.1)",
-    "A rabbit that constantly jumps around when you try to touch it",
-    new SimpleDate(2, 2, 2026),
-    "./work/Labs/5.1/catch_the_rabbit/catchTheRabbit.html"
+        "Catch the Rabbit (Lab 5.1.1)",
+        "A rabbit that constantly jumps around when you try to touch it",
+        new SimpleDate(2, 2, 2026),
+        "./work/Labs/5.1/catch_the_rabbit/catchTheRabbit.html"
     ),
 
     new Work(
-    "Arithmetic calculator (Lab 5.1.2)",
-    "2 input calculator that works with +, -, *, /, and %",
-    new SimpleDate(2, 2, 2026),
-    "./work/Labs/5.1/arithmetic/index.html"
+        "Arithmetic calculator (Lab 5.1.2)",
+        "2 input calculator that works with +, -, *, /, and %",
+        new SimpleDate(2, 2, 2026),
+        "./work/Labs/5.1/arithmetic/index.html"
     ),
 
     new Work(
-    "House Draw-er (Lab 6.1)",
-    "Draws \"houses\", \"cars\", and \"stars\" on request",
-    new SimpleDate(9, 2, 2026),
-    "./work/Labs/6.1/house_draw-er.html"
+        "House Draw-er (Lab 6.1)",
+        "Draws \"houses\", \"cars\", and \"stars\" on request",
+        new SimpleDate(9, 2, 2026),
+        "./work/Labs/6.1/house_draw-er.html"
     ),
 
     new Work(
-    "Guessing Game (Lab 7.1.1 + 7.1.2)",
-    "Has the user guess a number between 1-100, stores the object locally",
-    new SimpleDate(23, 2, 2026),
-    "./work/Labs/7.1/guessing_game/guessing_game.html"
+        "Guessing Game (Lab 7.1.1 + 7.1.2)",
+        "Has the user guess a number between 1-100, stores the object locally",
+        new SimpleDate(23, 2, 2026),
+        "./work/Labs/7.1/guessing_game/guessing_game.html"
     ),
 
     new Work(
-    "Random Moving Balls (Lab 7.1.3)",
-    "Generates 500 balls with random x,y positions, velocities, and colours",
-    new SimpleDate(23, 2, 2026),
-    "./work/Labs/7.1/moving_balls/balls.html"
+        "Random Moving Balls (Lab 7.1.3)",
+        "Generates 500 balls with random x,y positions, velocities, and colours",
+        new SimpleDate(23, 2, 2026),
+        "./work/Labs/7.1/moving_balls/balls.html"
     ),
 
     new Work(
-    "Incremental Game Assignment",
-    "Duck clicker, similar to cookie clicker",
-    new SimpleDate(28, 2, 2026),
-    "./work/increments/index.html"
+        "Incremental Game Assignment",
+        "Duck clicker, similar to cookie clicker",
+        new SimpleDate(28, 2, 2026),
+        "./work/increments/index.html"
     ),
 
     new Work(
-    "Simple Animation Altering (Lab 8.2.1)",
-    "Small ball begins as black slowly becomes red, moves down and left, and shrinks in radius",
-    new SimpleDate(4, 3, 2026),
-    "./work/Labs/8.2/anim.html"
+        "Simple Animation Altering (Lab 8.2.1)",
+        "Small ball begins as black slowly becomes red, moves down and left, and shrinks in radius",
+        new SimpleDate(4, 3, 2026),
+        "./work/Labs/8.2/anim.html"
     ),
 
     new Work(
-    "Team Logo Animation (Lab 8.2.2)",
-    "Logo for The Goober Development Group",
-    new SimpleDate(4, 3, 2026),
-    "./work/Labs/8.2/logo_anim.html"
+        "Team Logo Animation (Lab 8.2.2)",
+        "Logo for The Goober Development Group",
+        new SimpleDate(4, 3, 2026),
+        "./work/Labs/8.2/logo_anim.html"
+    ),
+
+    new Work(
+        "(Lab 9.1.1)",
+        "",
+        new SimpleDate(9, 3, 26),
+        "./work/Labs/9.1/slot/slot.php"
+    ),
+
+    new Work(
+        "(Lab 9.1.2)",
+        "",
+        new SimpleDate(9, 3, 26),
+        "./work/Labs/9.1/2/"
     ),
 
 
     new Work(
-    "JS Assignment",
-    "Unknown Yet",
-    new SimpleDate(0, 0, 0),
-    "./work/js_assignment/index.html"
+        "JS Assignment",
+        "Unknown Yet",
+        new SimpleDate(0, 0, 0),
+        "./work/js_assignment/index.html"
     ),
 
     new Work(
-    "The Goober Development Group",
-    "CS 1XD3 course team page",
-    new SimpleDate(1, 2, 2026),
-    "https://cs1xd3.cas.mcmaster.ca/~paten164/team"
+        "The Goober Development Group",
+        "CS 1XD3 course team page",
+        new SimpleDate(1, 2, 2026),
+        "https://cs1xd3.cas.mcmaster.ca/~paten164/team"
     ),
 ];
 
---
+++ work/Labs/9.1/slot/slot.php
@@ -0,0 +1,90 @@
+<!--
+Author : Alexander Perlock
+MACID : perlocka
+
+Date Created     : 
+Date Modified    : 
+
+What page is for
+-->
+<?php
+$img1 = 1;
+$img2 = 1;
+$img3 = 1;
+$display = "";
+function pull_slot()
+{
+    $pull = fn() => rand(1, 7);
+
+    global $img1, $img2, $img3, $display;
+
+    $img1 = $pull();
+    $img2 = $pull();
+    $img3 = $pull();
+
+    if ($img1 == $img2 && $img2 == $img3) {
+        // all 3 equal
+        $display = "JACKPOT!";
+    } else if ($img1 == $img2 || $img1 == $img3 || $img2 == $img3) {
+        // 2 equal
+        $display = "You've won!";
+    } else {
+        // none equal
+        $display = "Unfortunate";
+    }
+}
+
+pull_slot();
+?>
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Slot Machine</title>
+
+    <style>
+        body {
+            background-color: white;
+        }
+        h1 {
+            font-size: x-large;
+            text-align: center;
+        }
+        #box {
+            align-self: center;
+            display: flex;
+            justify-content: space-evenly;
+        }
+
+        img {
+            margin: 5%;
+            width: 15%;
+            height: 350px;
+        }
+
+        input {
+            width: 100%;
+            padding: 4%
+        }
+    </style>
+
+</head>
+
+<body>
+    <h1><?= $display ?></h1>
+    <div id="box">
+        <img src=<?= "./images/$img1.png" ?>>
+        <img src=<?= "./images/$img2.png" ?>>
+        <img src=<?= "./images/$img3.png" ?>>
+    </div>
+    <form method="post">
+        <input type="submit" value="Click to play again">
+    </form>
+
+</body>
+
+</html>
