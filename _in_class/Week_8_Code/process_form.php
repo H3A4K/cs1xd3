deb0f071e12f878d81f20525526813afce876772
H3A4K <alex.perlock@gmail.com>
2026-03-09 14:28:12 -0400

    inclass 9

--- _in_class/Week_8_Code/process_form.php
+++ _in_class/Week_8_Code/process_form.php
@@ -1,5 +1,17 @@
 <!DOCTYPE html>
 <!-- A very simple PHP Template. Sam Scott, McMaster University, 2025 -->
+<?php
+    $username = filter_input(INPUT_GET, "userid", FILTER_SANITIZE_SPECIAL_CHARS);
+    $age = filter_input(INPUT_GET, "age", FILTER_VALIDATE_INT);
+    $error = false;
+
+    if ($username === null) {
+        $error = true;
+    }
+    if ($age === null or $age === false) {
+        $error = true;
+    }
+?>
 <html>
 
 <head>
@@ -9,8 +21,12 @@
 </head>
 
 <body>
+    <p>Hello <?= $username ?></p>
+    <p>In 10 years you will be <?= $age + 10 ?></p>
     <?php
-    // put some code here
+    if ($error) {
+        echo "ERROR";
+    }
     ?>
 </body>
 
